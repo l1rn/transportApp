099668124cf4c89110a542a49aeda2604416d2f1
l1rn <ivtaks06@gmail.com>
2025-02-20 23:31:39 +0500

    jwt

--- src/main/java/com/example/transport_marketplace/jwt/JwtUtil.java
+++ src/main/java/com/example/transport_marketplace/jwt/JwtUtil.java
@@ -1,13 +1,36 @@
 package com.example.transport_marketplace.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
 
 public class JwtUtil {
     private static final String SECRET_KEY = "key";
 
     public static String generateToken(String username){
         return Jwts.builder()
-                .setSubject()
-                .setIssuedAt()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+    public static String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public static boolean validateToken(String token, String username){
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+    private static Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+    private static boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
     }
 }
