89b339388965da4f4e0f1b0459414d9ad4258b2d
j1nx qqe <ivtaks06@gmail.com>
2025-03-12 03:09:01 +0500

    test

--- src/test/java/com/example/transport_marketplace/TransportMarketplaceApplicationTests.java
+++ src/test/java/com/example/transport_marketplace/TransportMarketplaceApplicationTests.java
@@ -2,8 +2,14 @@ package com.example.transport_marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = TransportMarketplaceApplication.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = {
+				"spring.datasource.url=jdbc:h2:mem:testdb",
+				"spring.datasource.driver-class-name=org.h2.Driver",
+				"spring.h2.console.enabled=true"
+		}
+)
 class TransportMarketplaceApplicationTests {
 
 	@Test
