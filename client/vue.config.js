bddb082d5cdb139ccac0c97d30c526780d7a831c
j1nx qqe <ivtaks06@gmail.com>
2025-03-14 23:41:30 +0500

    fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: config => {
-    config.module
-      .rule('fonts')
-      .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
-      .use('url-loader')
-      .loader('url-loader')
-      .options({
-        limit: 4096,
-        name: 'fonts/[name].[hash:8].[ext]'
-      })
-  }
-  
+  transpileDependencies: true
 })
