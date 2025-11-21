892e37ba5135ebf83f6e25d1fba0e363e8d3c65b
l1rn <ivtaks06@gmail.com>
2025-11-21 13:12:48 +0500

    modal form close button

--- client/src/components/atom/ModalFormView.vue
+++ client/src/components/atom/ModalFormView.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="email-form">
-        <div 
-        ref="formRef"
-        class="email-form-container">
+        <div ref="formRef" class="email-form-container">
+            <div class="header-container">
+                <div 
+                @click="modalStore.close(props.storeKey)"
+                class="close-container">
+                    <img src="../../assets/icons/close.png" alt="">
+                </div>
+            </div>
             <div class="text-container">
                 <div class="icon">
-                    <img 
-                    :src="props.icon"
-                    alt="email">
+                    <img :src="props.icon" alt="email">
                 </div>
                 <div class="title">
                     {{ props.title }}
@@ -17,11 +20,7 @@
                 </div>
             </div>
             <div class="input-container">
-                <input 
-                v-model="localValue"
-                :type="props.inputType"
-                :placeholder="props.inputPlaceholder"
-                >
+                <input v-model="localValue" :type="props.inputType" :placeholder="props.inputPlaceholder">
                 <button @click="props.submitFunc">
                     {{ props.buttonName }}
                 </button>
@@ -57,48 +56,76 @@ useConditionalClickOutside(
 .email-form {
     position: relative;
     @include mixins.display-center();
+    flex-direction: column;
     height: 100%;
-    .email-form-container{
+
+    .header-container {
+        width: 90%;
+        .close-container {
+            display: flex;
+            align-items: start;
+            justify-content: end;
+            cursor: pointer;
+            img {
+                width: 16px;
+                height: 16px;
+                transition: all 0.3s ease;
+
+                &:hover {
+                    transform: rotate(90deg);
+
+                }
+            }
+        }
+    }
+
+    .email-form-container {
         background: colors.$white;
         width: 80%;
         max-width: 700px;
-        max-height: 500px;
+        max-height: 450px;
         height: 100%;
         @include mixins.display-column(center, center);
         border-radius: 24px;
         gap: 2rem;
-        .text-container{
+
+        .text-container {
             @include mixins.display-column(center, center);
             text-align: center;
             gap: 0.75rem;
-            .icon{
-                img{
+
+            .icon {
+                img {
                     width: 56px;
                 }
             }
-            .title{
+
+            .title {
                 font-size: 24px;
             }
-            .desc{
+
+            .desc {
                 width: 80%;
                 font-size: 18px;
             }
         }
+
         .input-container {
             @include mixins.display-column();
             gap: 1.25rem;
+
             input {
                 @include mixins.custom-input();
-            }  
+            }
+
             button {
-                @include mixins.button-clear(
-                    colors.$accent, 
-                    colors.$white
-                );
+                @include mixins.button-clear(colors.$accent,
+                    colors.$white );
                 padding: 0.75rem 1.25rem;
                 border-radius: 8px;
                 font-size: 1.15rem;
                 transition: all 0.3s ease;
+
                 &:hover {
                     background: colors.$primary-blue;
                 }
